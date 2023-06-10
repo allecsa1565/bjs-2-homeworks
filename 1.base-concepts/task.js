eaa721e82e08057bd377daf33abb831044be2bc0
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-10 21:43:38 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -8,3 +8,28 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
+"use strict"
+function solveEquation(a, b, c) {
+  let arr = [];
+  let d = b**2-4*a*c;
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d))/(2*a);
+    let x2 = (-b - Math.sqrt(d))/(2*a);
+    arr.push(x1, x2);
+  } else if (d == 0) {
+      let x1 = -b/(2*a);
+      arr.push(x1);
+  }
+  console.log(arr);
+  return arr;
+}
+
+solveEquation(1,2,4);
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+
+  // код для задачи №2 писать здесь
+
+  return totalAmount;
+}
