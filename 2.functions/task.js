cf10d8e7209b61e36f45bb2eb23ad97d40418cc6
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-12 21:07:59 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,49 @@
-function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+﻿'use strict'
+// Задание 1
+function getArrayParams(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  avg = parseFloat((sum / arr.length).toFixed(2));
+  return { min:min, max:max, avg:avg };
 }
 
-function summElementsWorker(...arr) {
-
+// Задание 2
+function worker(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
 }
 
-function differenceMaxMinWorker(...arr) {
-
+function makeWork(arrOfArr, func) {
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let arrSum = func(arrOfArr[i]);
+    if (arrSum > max) {
+      max = arrSum;
+    }
+  }
+  return max;
 }
+/* при отладке кода в браузере представлены другие массивы, нежели те что записаны в автотестах:
+console.log(makeWork([[10,10,20],[20,19,10]],worker)); =>49  вместо 15
+console.log(makeWork([[12,23,31],[41,5,61]],worker));  =>107 вместо 31
+console.log(makeWork([[14,21,36],[15,26,39]],worker)); =>80  вместо 0
+*/
 
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
+// Задание 3
+function worker2(arr) {
+  return getArrayParams(arr).max - getArrayParams(arr).min;
 }
