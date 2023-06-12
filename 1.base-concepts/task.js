829f59875389ebd61f44d9aa96e9d32676074081
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-12 20:48:53 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -21,29 +21,12 @@ else {
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let per = parseInt(percent) / 100 / 12; //процентная ставка
-    let con = parseInt(contribution); 	  //начальный взнос	
-    let am = parseInt(amount);  			  //общая стоимость	
-  	if (isNaN(per) || per < 0) {
-   		return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
-   	} else if (isNaN(con) || con < 0) {
-   		return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
-   	} else if (isNaN(am) || am < 0) {
-   		return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
-   	} else {
-			
-  		let today = new Date();
-  		if (today.getFullYear() > date.getFullYear() || today.getMonth() > date.getMonth()) {
-			return `Параметр "срок ипотеки" содержит неправильное значение ${date}`;
-		} else {
-			let s = am - con;						  //тело кредита
-  			let n = date.getMonth() - today.getMonth() + (12*(date.getFullYear() - today.getFullYear())); //срок кредита в месяцах
-  			let pay = s*(per+per/(((1+per)**n)-1));			//ежемесячная оплата
-  			let totalAmount = (pay * n).toFixed(2);
-  			console.log(totalAmount);
- 			return +totalAmount;
- 		}	
-    }
-
+function calculateTotalMortgage(percent, conntribution, amount, countMonths) {
+	let P - percent / 100 / 12;
+	let S - amount - contribution;
+	let n - countMonths;
+	
+	let summPerMonth - S * (P + (((1 + P)**n) - 1)));
+	let total - (summPerMonth*countMonths).tofixed(2);
+	return total;
 }
