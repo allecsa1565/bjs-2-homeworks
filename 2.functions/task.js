3124ed936da4caae8ce6d912763e02d957903eab
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-16 20:05:00 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,76 +1,53 @@
-'use strict'
-
-function getSolutions(a, b, c) {
-  const discriminant = b ** 2 - 4 * a * c;
-  if (discriminant > 0) {
-    let root = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
-    return {D: discriminant, roots: root};
-  } else if (discriminant === 0) {
-    let root = [-b / (2 * a)];
-    return {D: discriminant, roots: root};
+﻿'use strict'
+// Задание 1
+function getArrayParams(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
   }
-  return {D: discriminant, roots: []};
+  avg = parseFloat((sum / arr.length).toFixed(2));
+  return { min:min, max:max, avg:avg };
 }
 
-function showSolutionsMessage(a, b, c) {
-  const result = getSolutions(a, b, c);
-  (c < 0) ? console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x - ${Math.abs(c)}`)
-    : console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
-  console.log(`Значение дискриминанта: ${result.D}`);
-  if (result.D > 0)
-    console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
-  else if (result.D === 0)
-    console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
-  else
-    console.log(`Уравнение не имеет вещественных корней`);
+// Задание 2
+function worker(arr) {
+  let sum = 0;
+
+  arr.forEach(item => sum += arr[i]);
+
+  return sum;
 }
 
-console.log(getAverageScore({
-  algebra: [2, 4, 5, 2, 3, 4],
-  geometry: [2, 4, 5],
-  russian: [3, 3, 4, 5],
-  phisics: [5, 5],
-  music: [2, 2, 6],
-  english: [4, 4, 3],
-  poetry: [5, 3, 4],
-  chemistry: [2],
-  french: [4, 4]
-}))
+function makeWork(arrOfArr, func) {
+  let max = -Infinity;
 
-function getAverageMark(marks) {
-  if (marks.length === 0) {
-    return 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i]);
+    if (sum > max) max = sum;
   }
-  return (marks.reduce((curValue, prevValue) => {
-    return curValue + prevValue
-  }, 0)) / marks.length;
+
+  return max;
 }
 
-function getAverageScore(data) {
-  const newObj = {};
-  const arr = []
-  for (const key in data) {
-    newObj[key] = getAverageMark(data[key]);
-    arr.push(newObj[key]);
+// Задание 3
+function worker2(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
   }
-  newObj.average = getAverageMark(arr);
-  return newObj;
-}
 
-console.log(getPersonData({
-  aaa: 1,
-  bbb: 0
-}))
-
-function getDecodedValue(secret) {
-  if (secret === 1) {
-    return 'Эмильо';
-  }
-  if (secret === 0) {
-    return 'Родриго';
-  }
+  return max - min;
 }
-
-function getPersonData(secretData) {
-  return { firstName: getDecodedValue(secretData.aaa), lastName: getDecodedValue(secretData.bbb) }
-}
+   
