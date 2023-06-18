2f8ab8ee8ff9e2ee82163f46778c3dc57ca06651
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-19 02:09:39 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,64 +1,58 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
-// ############ TASK 1 ####################
-    min = arr[0];
-    max = arr[0];
-    sum = 0;
-    let arrLength = arr.length;
-    for (i = 0; i < arrLength; i += 1) {
-      if (arr[i] < min) { //ищем минимум
-        min = arr[i];
-      }
-      if (arr[i] > max) {//ищем максимум
-        max = arr[i];
-      }
-      sum = sum + arr[i]; //вычисляем сумму элементов массмва arr[]
-    }
+  let min, max, sum, avg;
+  min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min)
+      min = arr[i];
+  }
+  max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max)
+      max = arr[i];
+  }
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  avg = (sum / arr.length).toFixed(2);
+  avg = Number(avg);
 
-    avg = Number((sum / arrLength).toFixed(2)); //вычисляем среднее значение, обрезаем до 2-го знака после запятой, преобразуем в число
-
-  return { min:min, max:max, avg:avg };
+  return { min: min, max: max, avg: avg };
 }
 
-// ############# Задание 2 #################
+// Задание 2
 function worker(arr) {
-  let sum = 0;
-  let arrLength = arr.length;
-
-  for (i = 0; i < arrLength; i += 1) {
-    sum = sum + arr[i]; //вычисляем сумму элементов массмва arr[]
+  let sum;
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
   }
-
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max = -Infinity;
-  let arrOfArrLength = arrOfArr.length;
-
-  if (arrResultElement > max) {//ищем максимум
-        max = arrResultElement;
-      }
-
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let f = func(arrOfArr[i]);
+    if (f > max) {
+      max = f;
     }
+  }
   return max;
-)
+}
 
-// ############### Задание 3 ###############
-  let min = arr[0];
-    let max = arr[0];
-   let arrLength = arr.Length;
-    for (i = 0; i < arrLength; i += 1) {
-      if (arr[i] < min) { //ищем минимум
-        min = arr[i]; 
-      }
-      if (arr[i] > max) {//ищем максимум
-        max = arr[i]; 
-      }
+// Задание 3
+function worker2(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
     }
-
-    let delta = Math.abs(max - min); //вычисляем расстояние между мин и макс элементами массива arr[]
-
-    return delta;
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return Math.abs(max - min);
 }
