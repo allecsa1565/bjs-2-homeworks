a52c5a17c4df502b9a621dfe7e558a174938c3d3
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-18 22:25:33 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length)
+    return arr1.every((element, index) => element === arr2[index]);
+  else return false;
 }
+// let result = JSON.stringify(arr1)==JSON.stringify(arr2); //ещё один рабочий вариант
+// return result;
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function advancedFilter(arr) {
+  let result = arr.filter(i => i > 0).filter(i => i % 3 === 0).map(i => i * 10)
+  return result;
 }
