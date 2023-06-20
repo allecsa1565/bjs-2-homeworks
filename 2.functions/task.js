98a6e8dfc1fd9418b0833396b1a94ff23eca66e9
allecsa1565 <vacina.sasha15@gmail.com>
2023-06-20 21:44:20 +0300

    дополнила функции

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -16,25 +16,62 @@ function getArrayParams(...arr) {
 }
 
 // Задание 2
-function worker(arr) {
-  let sum;
-  sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
+
+function summElementsWorker(...arr) {
+  let summ = 0;
+  if (arr.length == 0) {
+    return summ = 0;
   }
-  return sum;
+  summ = arr.reduce((sum, item) => {
+    return sum += item;
+  });
+  return summ;
 }
 
-function makeWork(arrOfArr, func) {
-  let max = -Infinity;
-  for (let i = 0; i < arrOfArr.length; i++) {
-    let f = func(arrOfArr[i]);
-    if (f > max) {
-      max = f;
+function differenceEvenOddWorker(...arr) {
+  let sumEvenEl = 0,
+      sumOddEl = 0,
+      difElem = 0;
+      console.log(arr.length);
+  if (arr.length === 0) {
+    return difElem;
+  }
+  for (let i = 0 ; i < arr.length ; i++) {
+    if (arr[i] % 2 == 0) {
+      sumEvenEl += arr[i];
+    } else {
+      sumOddEl += arr[i]
     }
   }
-  return max;
+
+  return difElem = sumEvenEl - sumOddEl;
 }
+
+function averageEvenElementsWorker(...arr) {
+  let sumEvenEl = 0,
+      countEvenEl = 0;
+  if (arr.length === 0) {
+    return 0;
+  }
+  for (let i = 0 ; i < arr.length ; i++) {
+    if (arr[i] % 2 == 0) {
+      sumEvenEl += arr[i];
+      countEvenEl++;
+      }
+    }
+    return sumEvenEl / countEvenEl;
+  }
+
+  function makeWork (arrOfArr, func) {
+    let maxResult = -Infinity;
+    for(let i = 0 ; i < arrOfArr.length ; i++) {
+      let result = func(...arrOfArr[i]);
+      if(maxResult < result) {
+        maxResult = result
+      }
+    }
+    return maxResult;
+  }
 
 // Задание 3
 function worker2(arr) {
@@ -49,24 +86,4 @@ function worker2(arr) {
     }
   }
   return Math.abs(max - min);
-}
-
-function summElementsWorker(...arr) {
-
-}
-
-function differenceMaxMinWorker(...arr) {
-
-}
-
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
 }
