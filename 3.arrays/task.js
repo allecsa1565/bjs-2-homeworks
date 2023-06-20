21c3cc6c2f7e337621f12db4f97cf26285377874
allecsa1565 <vacina.sasha15@gmail.com>
2023-06-20 22:55:30 +0300

    переделывание работы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
-
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]); 
 }
 
-function advancedFilter(arr) {
-  let resultArr;
-  resultArr = arr
-  .filter((element) => element > 0) 
-  .filter((element) => element % 3 === 0) 
-  .map((element) => element * 10); 
-
-
-  return resultArr; // array
+function advancedFilter(arr1) {
+  return arr1.filter(item => item > 0 && item % 3 === 0).map(item => item * 10);
 }
