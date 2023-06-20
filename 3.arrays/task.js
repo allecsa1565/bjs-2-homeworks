338a14a8f361469edc4d0ff32b28798beb386a51
allecsa1565 <vacina.sasha15@gmail.com>
2023-06-20 23:02:29 +0300

    исправление 2 задания

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,10 @@ function compareArrays(arr1, arr2) {
   return arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]); 
 }
 
-function advancedFilter(arr1) {
-  return arr1.filter(item => item > 0 && item % 3 === 0).map(item => item * 10);
+function advancedFilter(arr) {
+  let resultArr;
+
+  // Ваш код
+  resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
+  return resultArr; // array
 }
