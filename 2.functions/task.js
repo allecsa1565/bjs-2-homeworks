2ea06004fd3d64b9dac9afd917ec741b9b2ce25e
allecsa1565 <vacina.sasha15@gmail.com>
2023-06-20 22:04:56 +0300

    переделывание работы

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,21 +1,25 @@
-// Задание 1
 function getArrayParams(...arr) {
-  let min = arr[0], max = arr[0], sum = 0, avg;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-    if (min > arr[i]) {
-      min = arr[i];
-    }
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  avg = (sum / arr.length).toFixed(2);
-  avg = Number(avg);
+  let min = Math.min.apply(null, arr),
+      max = Math.max.apply(null, arr),
+      avg = Number(arr.reduce(function (avg, item, index, arr) {
+        avg += item;
+        if (index === arr.length - 1) {
+          return avg / arr.length;
+        }
+        return avg;
+      }).toFixed(2));
+  // for (let i = 0 ; i < arr.length ; i++) {
+  //     if (arr[i] < min) {
+  //       min = arr[i]
+  //     }
+  //     if (arr[i] > max) {
+  //       max = arr[i]
+  //     }
+  //     avg += arr[i]
+  // }
+  // avg = +((avg / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
-
-// Задание 2
 
 function summElementsWorker(...arr) {
   let summ = 0;
@@ -37,16 +41,14 @@ function differenceMaxMinWorker(...arr) {
 
 }
 
-
 function differenceEvenOddWorker(...arr) {
-  let sumEvenEl = 0;
-      sumOddEl = 0;
+  let sumEvenEl = 0,
+      sumOddEl = 0,
       difElem = 0;
       console.log(arr.length);
   if (arr.length === 0) {
     return difElem;
   }
-}
   for (let i = 0 ; i < arr.length ; i++) {
     if (arr[i] % 2 == 0) {
       sumEvenEl += arr[i];
@@ -55,7 +57,8 @@ function differenceEvenOddWorker(...arr) {
     }
   }
 
-
+  return difElem = sumEvenEl - sumOddEl;
+}
 
 function averageEvenElementsWorker(...arr) {
   let sumEvenEl = 0,
@@ -72,29 +75,46 @@ function averageEvenElementsWorker(...arr) {
     return sumEvenEl / countEvenEl;
   }
 
-  function makeWork (arrOfArr, func) {
-    let maxResult = -Infinity;
-    for(let i = 0 ; i < arrOfArr.length ; i++) {
-      let result = func(...arrOfArr[i]);
-      if(maxResult < result) {
-        maxResult = result
-      }
-    }
-    return maxResult;
-  }
-
-// Задание 3
-function worker2(arr) {
-  let min = Infinity;
-  let max = -Infinity;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-    if (arr[i] > max) {
-      max = arr[i];
+function makeWork (arrOfArr, func) {
+  let maxResult = -Infinity;
+  for(let i = 0 ; i < arrOfArr.length ; i++) {
+    let result = func(...arrOfArr[i]);
+    if(maxResult < result) {
+      maxResult = result
     }
   }
- return Math.abs(max - min);
- }
+  return maxResult;
+}
 
+
+//
+// функции конструкторы
+
+// function newUser (name, lastName, age, sex) {
+//   this.name = name;
+//   this.lastName = lastName;
+//   this.age = age;
+//   this.sex = sex;
+//   this.greet = function() {
+//     console.log('Hi' + this.name + 'your age is ' + this.age);
+//   }
+// }
+//
+// const ivan = new newUser('Ivan', 'Ivanov', 35, 'male');
+// console.log(ivan);
+// let greetings = ivan.greet();
+// console.log(greetings);
+
+let arr = [];
+
+function newArr(valueOfNum) {
+  for(let i = 0 ; i < valueOfNum ; i++) {
+    arr.push(Math.round(Math.random() * 100));
+  }
+}
+
+newArr(5);
+let strArr = ['ivan', 'sasha', 'korol', 'tema']
+console.log(arr)
+strArr.map((item) => item[0])
+console.log(strArr);
