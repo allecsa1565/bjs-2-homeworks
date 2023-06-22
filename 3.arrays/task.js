615302b74d320fbf3e190b95759d9228f5870af2
allecsa1565 <vacina.sasha15@gmail.com>
2023-06-22 21:40:05 +0300

    исправление 2 задания

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,10 +3,21 @@ function compareArrays(arr1, arr2) {
 }
 
 function advancedFilter(arr) {
-	let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
-
-	// Ваш код
-
+	let resultArr;
+  
+	resultArr = arr
+	.filter((element) => element > 0) // оставляем токмо эдементы строго больше нуля
+	.filter((element) => element % 3 === 0) // оставляем токмо кратные трём
+	.map((element) => element * 10); // оставшиеся элементы умножаем на 10 и формируем из них массив
+  
 	return resultArr; // array
-	return arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
-}
+  }
+
+// function advancedFilter(arr) {
+//   return arr.reduce((acc, value) => {
+//     if (value > 0 && value % 3 === 0) {
+//       acc.push(value * 10)
+//     }
+//     return acc;
+//   }, [])
+// }
