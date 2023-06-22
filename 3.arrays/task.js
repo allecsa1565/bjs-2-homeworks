ebec48b265ecc6d1e5ace18a96561f10958f83d9
allecsa1565 <vacina.sasha15@gmail.com>
2023-06-22 22:46:28 +0300

    исправление

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,14 +2,14 @@ function compareArrays(arr1, arr2) {
 	return arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]); 
   }
 
-function getUsersNamesInAgeRange (arr) {
-	return arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10)
+  function getUsersNamesInAgeRange(users, gender) {
+	return gender.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10);
   }
   
-  // function getUsersNamesInAgeRange (arr) {
+  // function advancedFilter (arr) {
   //   return arr.reduce((acc, value) => {
   //     if (value > 0 && value % 3 === 0) {
-  //       acc.push(value * 10)
+  //       acc.push(value * 10);
   //     }
   //     return acc;
   //   }, [])
