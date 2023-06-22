168f6e4df07e05a49607c8310b42b1247fb677e9
allecsa1565 <vacina.sasha15@gmail.com>
2023-06-22 22:29:11 +0300

    решение 2 задания

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,22 +1,16 @@
 function compareArrays(arr1, arr2) {
-  return arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]); 
-}
+	return arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]); 
+  }
 
 function advancedFilter(arr) {
-	let resultArr;
-  
-	resultArr = arr
-	.filter((element) => element > 0) // оставляем токмо эдементы строго больше нуля
-	.filter((element) => element % 3 === 0) // оставляем токмо кратные трём
-	.map((element) => element * 10); // оставшиеся элементы умножаем на 10 и формируем из них массив
-  
-// function advancedFilter(arr) {
-//   return arr.reduce((acc, value) => {
-//     if (value > 0 && value % 3 === 0) {
-//       acc.push(value * 10)
-//     }
-//     return acc;
-//   }, [])
-// }
-return resultArr; // array
+	return arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10)
   }
+  
+  // function advancedFilter(arr) {
+  //   return arr.reduce((acc, value) => {
+  //     if (value > 0 && value % 3 === 0) {
+  //       acc.push(value * 10)
+  //     }
+  //     return acc;
+  //   }, [])
+  // }
