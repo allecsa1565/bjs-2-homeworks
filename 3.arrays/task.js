92bca2d332e2474e8043f9833b9fc5764db06171
allecsa1565 <vacina.sasha15@gmail.com>
2023-06-22 23:03:07 +0300

    испраление грамматике

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,7 +2,7 @@ function compareArrays(arr1, arr2) {
 	return arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]); 
   }
 
-  function getUsersNamesInAgeRange(users, gender) {
+  function getUsersNamesInAgeRange(arr) {
 
    let result = arr.filter ((item) => item > 0 && item % 3 === 0).map((item) => item * 10);
   }
