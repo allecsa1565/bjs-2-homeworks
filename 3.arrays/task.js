521398e2acad9df7cc7066613e2a0848f0f5f5e1
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-24 22:44:00 +0300

    Исправление возвращение NaN

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,8 +2,11 @@ function compareArrays(arr1, arr2) {
 	return arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]); 
   }
 
-
- function getUsersNamesInAgeRange(users, genders) { 
+function getUsersNamesInAgeRange(users, genders) { 
     const ages = users.filter( user => user.gender === genders).map(user => user.age);
-    return ages.reduce((a, b) =>(a + b), 0) / ages.length;
+   const result = ages.reduce((a, b) =>(a + b), 0) / ages.length;
+   if (Number.isNaN(result)) {
+     return 0;
+   }
+    return result;
  }
