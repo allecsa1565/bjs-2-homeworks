2937c3d5653b0beaab2b0560975bf7b794e0b994
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-28 21:09:14 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,38 +1,29 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-  }
-  
-  Student.prototype.setSubject = function (subjectName) {
+  // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+};
+
+Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
-  }
-  
-  Student.prototype.addMark = function (mark) {
-    if (this.marks === undefined) {
-      this.marks = [mark];
+};
+
+
+Student.prototype.addMarks = function (...marks) {
+    if ('marks' in this === true) {
+        this.marks.push(...marks)
     }
-    else {
-      this.marks.push(mark);
-    }
-  }
-  
-  Student.prototype.addMarks = function (...rest) {
-    if (this.marks === undefined) {
-      this.marks = [...rest];
-    }
-    else {
-      this.marks.push(...rest);
-    }
-  }
-  
-  Student.prototype.getAverage = function () {
-    let sum = 0;
-    this.marks.forEach((mark) => {sum += parseInt(mark)});
-    return sum / this.marks.length;  
-  }
-  
-  Student.prototype.exclude = function (reason) {
+};
+
+
+Student.prototype.getAverage = function () {
+    return this.marks.reduce((acc, item) => acc += item) / this.marks.length;
+};
+
+
+Student.prototype.exclude = function (reason) {
     delete this.subject;
     delete this.marks;
     this.excluded = reason;
+};
