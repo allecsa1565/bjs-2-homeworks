ffd9520776db0ad2ecc28620900003a6172cada9
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-28 20:17:18 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,43 +1,56 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age= age;
-}
+  // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+};
 
 Student.prototype.setSubject = function (subjectName) {
+  //ваш код
   this.subject = subjectName;
-}
+};
+
+// ваш код для остальных методов
+
 
 Student.prototype.addMark = function (mark) {
-  if (this.marks === undefined) {
-    this.marks = [];
-    this.marks.push(mark);
-  } else {
-    this.marks.push(mark);
-  }
-}
+  //ваш код
 
-Student.prototype.addMarks = function(...marks) {
   if (this.marks === undefined) {
-    this.marks = [];
-    this.marks = [...this.marks, ...marks];
+    // добавить первую оценку 
+    this.marks = [mark];
+
   } else {
-    this.marks = [...this.marks, ...marks];
-  }
-}
+    // добавить вторую и последующие оценки в массив
+    this.marks.push(mark);
+  };
+};
+
+Student.prototype.addMarks = function (...mark) {
+  //ваш код
+
+  if (this.marks === undefined) {
+    // добавить первую оценку 
+    this.marks = [];
+    this.marks.push(...mark);
+
+  } else {
+    // добавить вторую и последующие оценки в массив
+    this.marks.push(...mark);
+  };
+};
+
 
 Student.prototype.getAverage = function () {
-  if (this.marks === undefined) {
-    return;
-  }
-  return this.marks.reduce((acc, value) => acc + value, 0) / this.marks.length;
-}
+    return this.marks.reduce((acc, item) => acc += item) / this.marks.length;
+};
 
-Student.prototype.excludeStudent = function(reason) {
-  if (this.subject === undefined) {
-    return 'Студент не участвует в учебном процессе!'
-  }
+
+Student.prototype.exclude = function (reason) {
+  //ваш код
+
   delete this.subject;
   delete this.marks;
+
   this.excluded = reason;
-}
+};
