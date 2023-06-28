7d0b21b4c140173ea676a64e9c6c9b0b23dd13b7
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-28 18:34:50 +0300

    исправление ошибок

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,29 +1,39 @@
 function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
-}
-
-Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
-}
-
-Student.prototype.addMark = function (mark) {
-  (this.marks === undefined) ? this.marks = [mark] : this.marks.push(mark);
-}
-
-Student.prototype.addMarks = function (...marks) {
-  (this.marks === undefined) ? this.marks = [...marks] : this.marks.push(...marks);
-}
-
-Student.prototype.getAverage = function () {
-  let sum = 0;
-  this.marks.forEach(item => sum += item);
-  return sum / this.marks.length;
-}
-
-Student.prototype.exclude = function (reason) {
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
+  
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
+  
+  Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+      this.marks = [mark];
+    }
+    else {
+      this.marks.push(mark);
+    }
+  }
+  
+  Student.prototype.addMarks = function (...rest) {
+    if (this.marks === undefined) {
+      this.marks = [...rest];
+    }
+    else {
+      this.marks.push(...rest);
+    }
+  }
+  
+  Student.prototype.getAverage = function () {
+    let sum = 0;
+    this.marks.forEach((mark) => {sum += parseInt(mark)});
+    return sum / this.marks.length;  
+  }
+  
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
