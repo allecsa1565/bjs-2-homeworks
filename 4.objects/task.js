47c14a0cd8b12c5cf5328e2436eb91bb9ccc2163
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-06-28 21:44:21 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,31 +1,40 @@
 function Student(name, gender, age) {
-  // Ваш код
   this.name = name;
   this.gender = gender;
   this.age = age;
-};
+  this.marks = [];
+}
 
 Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
-};
-
-
-Student.prototype.addMark = function (mark) {
-  (this.marks === undefined) ? this.marks = [mark] : this.marks.push(mark);
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  (this.marks === undefined) ? this.marks = [...marks] : this.marks.push(...marks);
+  if (this.marks) {
+    this.marks = [...this.marks, ...marks];
+  }
 }
 
-
 Student.prototype.getAverage = function () {
-    return this.marks.reduce((acc, item) => acc += item) / this.marks.length;
-};
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
 
+  return this.marks.reduce((acc, cur, idx, arr) => {
+    acc += cur;
+    if (idx === arr.length - 1) {
+      return acc / arr.length;
+    }
+
+    return acc;
+  }, 0)
+}
 
 Student.prototype.exclude = function (reason) {
-    delete this.subject;
-    delete this.marks;
-    this.excluded = reason;
-};
+  if (this.subject) delete this.subject;
+  if (this.marks) delete this.marks;
+  this.excluded = reason;
+}
+
+const student = new Student("Василиса", "женский", 19);
+console.log(student);
