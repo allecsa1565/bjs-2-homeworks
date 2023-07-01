91aa7042a84387e10732a65cdb3c1b9627b2758e
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-07-01 17:41:52 +0300

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,89 +1,72 @@
-function cachingDecoratorNew(func) {
-  
-  let cache =[];
+unction cachingDecoratorNew(func) {
+  let cashe = [];
+  let counter =0;
 
   function wrapper(...args) {
-    const hash = args.join(","); //формируем хэш(уникальный признак) способом строчного перечисления параметров чрз запятую
 
+    const hash = args.join(',');
 
-    let idx = cache.findIndex((item)=> item.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
-
-    if (idx !== -1 ) { // если элемент найден
-        console.log("Из кэша: " + cache[idx].value); // вывод в консоль нового вычисленного значения func
-        return "Из кэша: " + cache[idx].value;
-    } else {
-         let value = func(...args); // вычисление значения свойства .value, которое станет элементом массива cashe[]. 
-         cache.push({hash, value}) ; // добавляем элемент-объект с двумя свойствами в конец массива cashe[]
-    let cashLengh = cache.length; // вычисляем длину массива cashe[]
-      if (cashLengh > 5) { 
-          cache.shift(); // если элементов в кэше больше пяти - удалить самый старый (первый) 
-      }
-    console.log("Вычисляем: " + value);
-    return "Вычисляем: " + value;
-    }
-     
+    if (hash in cashe) {
+      return "Из кэша: " + cashe[hash];
   }
-  return wrapper;
-  
+  if (counter >= 5) {
+      delete cashe[Object.keys(cashe)[0]];
+  }
+  counter++;
+  cashe[hash] = func(...args);
+  return "Вычисляем: " + cashe[hash];
 }
 
- // #################################### TASK 2 ############################################
+return wrapper;
+}
+
+
 
 function debounceDecoratorNew(func, ms) {
- 
-  let timeout;
-  let flag = false, 
-  savedArgs,
-  savedThis;
+  let isThrottled = false;
+	let timeout;
 
-  return function (...args) {
-    
-    savedArgs = args; // обновляем аргументы при каждом вызове функции func
-    savedThis = this; // обновляем контекст при каждом вызове функции func
-    if (flag) { // если флаг поднят, выходим без запуска func
-      return;
-    }
+	function wrapper(...args) {
+		clearTimeout(timeout);
 
-    func.apply(this, savedArgs); // запускаем переданную функцию
-    flag = true; // взводим флаг
-    clearTimeout(timeout); //удаляем старый таймер
+		timeout = setTimeout(() => {
+			isThrottled = false;
+			func.apply(this, args);
+		}, ms);
 
-    timeout = setTimeout(() => { // ставим таймер,  
-    flag = false; // чтобы очистить флаг
-    func.apply(savedThis, savedArgs); // и выполнить func с последними сохранёнными аргументами и контекстом
-    }, ms);
-  }
- 
+		if (isThrottled) return;
+
+		func.apply(this, args);
+		isThrottled = true;
+	};
+	return wrapper;
+  
 }
 
-// #################################### TASK 3 ############################################
+function debounceDecorator2(func) {
+  let isThrottled = false;
+	let timeout;
+	wrapper.count = 0;
 
-function debounceDecorator2(func, ms) {
-  
-  let timeout;
-  let flag = false, 
-  savedArgs,
-  savedThis,
-  count; // счётчик вызова func
-  
+	function wrapper(...args) {
 
-  return function (...args) {
-    
-    if (count === undefined) count = 0; //при первом вызове func ставим счётчик на ноль
-    savedArgs = args; // обновляем аргументы при каждом вызове функции func
-    savedThis = this; // обновляем контекст при каждом вызове функции func
-    if (flag) { // если флаг поднят, выходим без запуска func
-      return;
-    }
+		clearTimeout(timeout);
 
-    func.apply(this, savedArgs); // запускаем переданную функцию
-    flag = true; // взводим флаг
-    count += 1 // увеличиваем счётчик вызова func на 1
-    clearTimeout(timeout); //удаляем старый таймер
+		timeout = setTimeout(() => {
+			isThrottled = false;
+			func.apply(this, args);
+			wrapper.count++;
+		}, ms);
 
-    timeout = setTimeout(() => { // ставим таймер,  
-    flag = false; // чтобы очистить флаг
-    func.apply(savedThis, savedArgs); // выполнить func с последними сохранёнными аргументами и контекстом,
-    count += 1; // и увеличить счётчик вызова func на 1
-    }, ms);
-  }
+		if (isThrottled) {
+			return;
+		};
+
+		func.apply(this, args);
+		wrapper.count++;
+		isThrottled = true;
+
+		console.log(wrapper.count);
+	};
+	return wrapper;
+}
